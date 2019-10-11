10c812fa9c0b00c4cbe27d3e6f987584fd827550
Jeff Kelly <jekelly@microsoft.com>
2019-10-11 16:32:41 -0700

    Add project files.

---
+++ ScrollTester/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScrollTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 1000; i++)
+            {
+                this.Horizontal.Items.Add(new ListBoxItem() { Content = $"item {i}" });
+                this.Vertical.Items.Add(new ListBoxItem() { Content = $"item {i}" });
+            }
+        }
+    }
+}
